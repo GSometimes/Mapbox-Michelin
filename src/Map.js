2da25ff66d1d06b6d10ef548ea891795c8f728bc
Gourav <gore.auluck@gmail.com>
2021-03-09 23:20:11 -0800

    Adding points on Map

--- src/Map.js
+++ src/Map.js
@@ -4,13 +4,34 @@ import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NvbWV0aW1lcyIsImEiOiJja20xbnF1ZWgwa2FjMm9tOXBpeWcxZ3l1In0.xtREw0TINY-mVkKPhIFy9Q'
 
+const data = [
+    {
+        "location": "Staples Center",
+        "city": "Los Angeles",
+        "state": "California",
+        "coordinates": [-118.2673, 34.0430]
+    },
+    {
+        "location": "Chinatown",
+        "city": "Los Angeles",
+        "state": "California",
+        "coordinates": [-118.2383, 34.0623]
+    },
+    {
+        "location": "Dodger Stadium",
+        "city": "Los Angeles",
+        "state": "California",
+        "coordinates": [-118.2400, 34.0739]
+    }
+]
+
 class Mapp extends Component {
     constructor(props){
         super(props);
         this.state = {
-            lng: -117.8532,
-            lat: 34.0297,
-            zoom: 8.70
+            lng: -118.2437,
+            lat: 34.0522,
+            zoom: 12
         };
     }
 
@@ -21,6 +42,14 @@ class Mapp extends Component {
             center: [this.state.lng, this.state.lat],
             zoom: this.state.zoom
         })
+
+        data.forEach((location) => {
+            console.log(location)
+            let marker = new mapboxgl.Marker()
+            .setLngLat(location.coordinates)
+            .addTo(map);
+        })
+
     }
 
     render(){
